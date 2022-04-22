b3534f2f6bd8a4b83191e7d902e21639e04bf373
Liangjie Shen <rogershen.sr@gmail.com>
2022-04-22 15:48:41 -0700

    Update newtonMethodRandom.cpp

--- newtonMethodRandom.cpp
+++ newtonMethodRandom.cpp
@@ -8,8 +8,8 @@
 using namespace std;
 
 // global variables
-int MIN = 0;
-int MAX = 1000;
+int MIN = -100;
+int MAX = 100;
 int n;
 
 // method declaration
@@ -19,32 +19,107 @@ double EvalNewton(double[],double[],float);
 // driver method
 int main(){
     
-    
-
     /* initialize random seed: */
     srand (time(NULL));
 
     cout << "Please enter the size of the random data: ";
     cin >> n;
 
+    // Create an empty coefficient vector and initialize arrays and variables
+    double cs[n],xs[n],ys[n];
+    float z;
+
+    // write random points to file
     random_device rd;
     default_random_engine eng(rd());
     uniform_real_distribution<double> distr(MIN, MAX);
 
-    ofstream file;
+    ofstream myfile;
+    
+    myfile.open("Random.pnt");
+    
+    for(int i = 0; i < n; i++){
+        myfile << distr(eng) << " ";
+    }
+
+    myfile << "\n";
+
+    for(int i = 0; i < n; i++){
+        myfile << distr(eng) << " ";
+    }
+
+    myfile.close();
+
+    // read numbers to file
+    ifstream file; // call read file function
     file.open("Random.pnt");
+    double y = -1;
+    int counter = 0;
+   
     for(int i = 0; i < n; i++){
-        file << distr(eng) << " ";
+    
+        file >> y;
+        xs[counter] = y;
+        counter++;
+
+        
     }
 
-    file << "\n";
+    counter = 0;
+    for(int i = n; i < n*2; i++){
+        
+        file >> y;
+        ys[counter] = y;
+        counter++;
 
-    for(int i = 0; i < n; i++){
-        file << distr(eng) << " ";
     }
+
     file.close();
 
-    
+    string q;
+    bool valid = false;
+
+    while(!valid){
+        cout << "Please enter the point to be evaluated at: x = [enter q if you wanted to quit]";
+        cin >> q;
+
+        if(q == "q"){
+            exit(0);
+        }
+
+        else{
+            try
+            {
+                z = stod(q);
+                valid = true;
+            }
+            catch(...)
+            {
+                cout << "Invalid evluation point entered!" << endl;
+            }
+                
+        }
+    }
+
+    clock_t start = clock (); // time starts
+        
+    Coeff(xs,ys,cs);
+
+    cout << endl;
+    // time ends and time taken are calculated
+    cout << "Time Taken for Coeff(xs,ys,cs): " << 
+            (((float)( clock() - start )) / CLOCKS_PER_SEC)*1000000000 << " nano seconds." << endl;
+        
+
+    start = clock ();
+
+    cout << endl;
+    cout << "f(" << z << ") = " << EvalNewton(xs,cs,z) << endl;
+    cout << endl;
+
+    // time ends and time taken are calculated
+    cout << "Time Taken for EvalNewton(xs,cs,z): " << 
+            (((float)( clock() - start )) / CLOCKS_PER_SEC)*1000000000 << " nano seconds." << endl;
 }
 
 void Coeff(double xs[n+1] , double ys[n+1], double cs[n+1]){
